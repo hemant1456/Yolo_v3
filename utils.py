f4cff3f6a80bc3db616c668366832a0cdd4b46da
hemant <hkbhambhu@gmail.com>
2024-03-04 12:36:54 +0530

    added intersection over union function

---
+++ utils.py
@@ -0,0 +1,40 @@
+import torch
+
+def calculate_area(x1, y1, x2, y2):
+    area = (x2-x1).clamp(0) * (y2-y1).clamp(0)
+    return area
+
+def intersection_over_union(boxes_preds, boxes_labels, box_format="midpoint"):
+    #boxes preds shape (N,4) and boxes label shape is (N,4)
+    if box_format=="corners":
+        box1_x1 = boxes_preds[...,0:1]
+        box1_x2 = boxes_preds[...,1:2]
+        box1_y1 = boxes_preds[...,2:3]
+        box1_y2 = boxes_preds[...,3:4]
+
+        box2_x1 = boxes_labels[...,0:1]
+        box2_x2 = boxes_labels[...,1:2]
+        box2_y1 = boxes_labels[...,2:3]
+        box2_y2 = boxes_labels[...,3:4]
+    elif box_format=="midpoint":
+        box1_x1 = boxes_preds[...,0:1] - boxes_preds[...,2:3]/2
+        box1_x2 = boxes_preds[...,0:1] + boxes_preds[...,2:3]/2
+        box1_y1 = boxes_preds[...,1:2] - boxes_preds[...,3:4]/2
+        box1_y2 = boxes_preds[...,1:2] + boxes_preds[...,3:4]/2
+
+        box2_x1 = boxes_labels[...,0:1] - boxes_labels[...,2:3]/2
+        box2_x2 = boxes_labels[...,0:1] + boxes_labels[...,2:3]/2
+        box2_y1 = boxes_labels[...,1:2] - boxes_labels[...,3:4]/2
+        box2_y2 = boxes_labels[...,1:2] + boxes_labels[...,3:4]/2
+
+    x1 = torch.max(box1_x1, box2_x1)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
+
+    intersection = calculate_area(x1, y1, x2, y2)
+    box1_area = calculate_area(box1_x1, box1_y1, box1_x2, box1_y2)
+    box2_area = calculate_area(box2_x1, box2_y1, box2_x2, box2_y2)
+    union = box1_area + box2_area - intersection + 1e-6
+
+    return intersection/union
